53f8045b87a4c4425c1eb227ebe3b05490b31eb3
slayonce <katharinemg@gmail.com>
2023-04-04 23:20:29 -0400

    Added js code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 "use strict";
 
-// VARIABLES
+// DEFINE VARIABLES
 let pwLength;
 let lowercase = "abcdefghijklmnopqrstuvwxyz";
 let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -11,54 +11,92 @@ let confirmLowercase;
 let confirmUppercase;
 let confirmNumeric;
 let confirmSpecialChar;
-let charSelections;
+let charSelections = "";
+let password = "";
 
-// Get references to the #generate element ORIGINAL
-// var generateBtn = document.querySelector("#generate");
-document
-  .querySelector("#generate")
-  .addEventListener("click", function selectOptions() {
-    // Prompt to enter password length
-    let pwLength = parseInt(
-      prompt(
-        `What is the desired length of your password? Please type a number between 8 and 128.`
-      )
+// // Get references to the #generate element ORIGINAL
+var generateBtn = document.querySelector("#generate");
+
+// // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// // Write password to the #password input ORIGINAL
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  // if (generatePassword) {
+  passwordText.value = password;
+  // } else return;
+}
+
+// PROMPT TO ENTER PASSWORD LENGTH CONVERTING TO NUMBERS
+// function selectOptions() {
+function generatePassword() {
+  let pwLength = parseInt(
+    prompt(
+      `What is the desired length of your password? Please type a number between 8 and 128.`
+    )
+  );
+  // Test for criteria
+  if (isNaN(pwLength) || pwLength < 8 || pwLength > 128) {
+    alert(`Oh no! Your password is not a valid length, please try again!`);
+    // return error message
+    return Error(`Invalid length, please try again.`);
+  } else confirm(`Great! Your password will be ${pwLength} characters long.`);
+
+  // Confirm lowercase letters
+  confirmLowercase = confirm(
+    `Would you like to include lowercase letters in your password? If yes, click "🆗"! If no, click "Cancel".`
+  );
+  // Confirm uppercase letters
+  confirmUppercase =
+    confirm(`Would you like to include uppercase letters in your password? If yes, click "🆗"! if no, click "Cancel".
+    `);
+
+  // Confirm numeric
+  confirmNumeric =
+    confirm(`Would you like to include numbers in your password? If yes, click "🆗"! If no, click "Cancel".
+    `);
+  //Confirm Special Characters
+  confirmSpecialChar =
+    confirm(`Would you like to include special characters in your password? If yes, click "🆗"! If no, click "Cancel".
+    `);
+  // Make sure one character type was selected
+  if (
+    !confirmLowercase &&
+    !confirmUppercase &&
+    !confirmNumeric &&
+    !confirmSpecialChar
+  ) {
+    alert(
+      `Oh no! No character sets were selected. Please try again, confirming at least one of the character sets!`
     );
-    // Test for criteria
-    if (isNaN(pwLength) || pwLength < 8 || pwLength > 128) {
-      alert(
-        `Oh no! Your password is not a valid length, please type a number between 8 and 128.`
-      );
-      return;
-    } else confirm(`Great! Your password will be ${pwLength} characters long.`);
-
-    // Confirm lowercase letters
-    confirmLowercase = confirm(
-      `Would you like to include lowercase letters in your password? If yes, click "Ok", if no, click "Cancel"!`
+    return Error(
+      `Please choose at least one character set: 
+      Lowercase: ${lowercase}
+      Uppercase: ${uppercase}
+      Numeric: ${numeric}
+      Special character: ${specialChar})`
     );
-    // Confirm uppercase letters
-    confirmUppercase =
-      confirm(`Would you like to include uppercase letters in your password? If yes, click "Ok", if no, click "Cancel"!.
-    `);
-
-    // Confirm numeric
-    confirmNumeric =
-      confirm(`Would you like to include numbers in your password? If yes, click "Ok", if no, click "Cancel"!.
-    `);
-    //Confirm Special Characters
-    confirmSpecialChar =
-      confirm(`Would you like to include special characters in your password? If yes, click "Ok", if no, click "Cancel"!
-    `);
-    alert(`Great! We are generating your password!`);
-  });
-
-// Write password to the #password input ORIGINAL
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
-
-// Add event listener to generate button ORIGINAL
-// generateBtn.addEventListener("click", writePassword);
+  } else {
+    alert(`Great! We are generating your secure password!`);
+  }
+  if (confirmLowercase) {
+    charSelections += lowercase;
+  }
+  if (confirmUppercase) {
+    charSelections += uppercase;
+  }
+  if (confirmNumeric) {
+    charSelections += numeric;
+  }
+  if (confirmSpecialChar) {
+    charSelections += specialChar;
+  }
+  for (let i = 0; i < pwLength; i++) {
+    password += charSelections.charAt(
+      Math.floor(Math.random() * charSelections.length)
+    );
+  }
+  return password;
+}
