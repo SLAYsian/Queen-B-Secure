850381759d6388491e6662cf528273116d3d881b
slayonce <katharinemg@gmail.com>
2023-04-06 22:17:17 -0400

    updated readme and added images of project to assets

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # QUEEN B SECURE
+
 Secure Password Generator
+
+## Author:
+
+Kat Gorecki
+
+### User Story:
+
+As an employee, I want to be able to generate a random, secure password, based on criteria I choose.
+
+### Usage:
+
+- Click "Generate Password" button
+- Follow the series of prompts
+- Enter a password length that is betweed 8 and 128 characters
+- Confirm whether you would like to include: lowercase, uppercase, numbers, and special characters. You must confirm at least one of these sets. Click "Ok" for yes and "Cancel" for no.
+- Once you have made your selection, click "Ok" and your password will appear in the "Your Secure Password" box.
+- Highlight and copy to use!
+
+[Live](https://slayonce.github.io/Queen-B-Secure/)
+
+### Screenshots:
+
+![./assets/confirm-length.png]
+![./assets/enter-length.png]
+![./assets/length-conf-alert.png]
+![./assets/confirm-lowercase.png]
+![./assets/confirm-uppercase.png]
+![./assets/confirm-numbers.png]
+![./assets/confirm-special-character.png]
+![./assets/confirmation-alert.png]
+![./assets/password-generated.png]
+
+### References
+
+1. MDN (https://developer.mozilla.org/en-US/)
+2. Stack Overflow (https://stackoverflow.co/)
+3. Jonas Schmedtmann courses (https://codingheroes.io/resources/)
+
+### License
+
+NA
--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Assignment code here
 "use strict";
 
-// DEFINE VARIABLES
+// NOTES DEFINE VARIABLES
 let pwLength;
 let lowercase = "abcdefghijklmnopqrstuvwxyz";
 let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let numeric = "0123456789";
-let specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~"; // missing the \ and space
+let specialChar = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 let confirmLowercase;
 let confirmUppercase;
 let confirmNumeric;
@@ -17,57 +17,56 @@ let password = "";
 // // Get references to the #generate element ORIGINAL
 var generateBtn = document.querySelector("#generate");
 
-// // Add event listener to generate button
+// // Add event listener to generate button ORIGINAL
 generateBtn.addEventListener("click", writePassword);
 
 // // Write password to the #password input ORIGINAL
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  // if (generatePassword) {
   passwordText.value = password;
-  // } else return;
 }
 
-// PROMPT TO ENTER PASSWORD LENGTH CONVERTING TO NUMBERS
-// function selectOptions() {
 function generatePassword() {
+  // NOTES PROMPT TO ENTER PASSWORD LENGTH CONVERTING TO NUMBERS
   let pwLength = parseInt(
     prompt(
       `What is the desired length of your password? Please type a number between 8 and 128.`
     )
   );
-  // Test for criteria
+  // NOTES TEST FOR CRITERIA
   if (isNaN(pwLength) || pwLength < 8 || pwLength > 128) {
     alert(`Oh no! Your password is not a valid length, please try again!`);
-    // return error message
+    // NOTES RETURN ERROR MESSAGE
     return Error(`Invalid length, please try again.`);
+    //NOTES Return confirmation
   } else confirm(`Great! Your password will be ${pwLength} characters long.`);
 
-  // Confirm lowercase letters
+  // NOTES Confirm lowercase letters
   confirmLowercase = confirm(
     `Would you like to include lowercase letters in your password? If yes, click "🆗"! If no, click "Cancel".`
   );
-  // Confirm uppercase letters
+  // NOTES Confirm uppercase letters
   confirmUppercase =
     confirm(`Would you like to include uppercase letters in your password? If yes, click "🆗"! if no, click "Cancel".
     `);
 
-  // Confirm numeric
+  // NOTES Confirm numeric
   confirmNumeric =
     confirm(`Would you like to include numbers in your password? If yes, click "🆗"! If no, click "Cancel".
     `);
-  //Confirm Special Characters
+  // NOTES Confirm Special Characters
   confirmSpecialChar =
     confirm(`Would you like to include special characters in your password? If yes, click "🆗"! If no, click "Cancel".
     `);
-  // Make sure one character type was selected
+  // NOTES Make sure one character type was selected
   if (
     !confirmLowercase &&
     !confirmUppercase &&
     !confirmNumeric &&
     !confirmSpecialChar
   ) {
+    // NOTES Alert with error message
     alert(
       `Oh no! No character sets were selected. Please try again, confirming at least one of the character sets!`
     );
@@ -81,6 +80,7 @@ function generatePassword() {
   } else {
     alert(`Great! We are generating your secure password!`);
   }
+  //NOTES: if confirm character set is true, add string to charSelections
   if (confirmLowercase) {
     charSelections += lowercase;
   }
@@ -93,6 +93,7 @@ function generatePassword() {
   if (confirmSpecialChar) {
     charSelections += specialChar;
   }
+  // NOTES: for loop using to genertate random password using the charSelections and pwLenth
   for (let i = 0; i < pwLength; i++) {
     password += charSelections.charAt(
       Math.floor(Math.random() * charSelections.length)
